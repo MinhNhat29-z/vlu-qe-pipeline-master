d3cbe63aaea76c09292c96a18eb47571a35bc493
Minh Nhật <minhnhat10206@gmail.com>
2026-09-21 14:36:44 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 function login(username, password) {
     // Login authentication
-    if (username === "admin" && password === "123") {
+    if (username === "admin" && password === "9999") {
         return true;
     }
 
